c529771dc72cd3c7157e687060b655d232735a36
nishanthini2136 <nishanthini2136@gmail.com>
2025-12-20 21:35:53 +0530

    Fix API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: true,
+  withCredentials: false,
 });
 
 export default API;
