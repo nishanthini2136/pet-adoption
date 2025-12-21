fe22b34c84562d45e188c3e55132a3e4607238d1
nishanthini2136 <nishanthini2136@gmail.com>
2025-12-21 21:08:01 +0530

    Fix API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000",
+  // Support both env names in case the deployment used REACT_APP_API_URL
+  baseURL:
+    process.env.REACT_APP_API_BASE_URL ||
+    process.env.REACT_APP_API_URL ||
+    "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
